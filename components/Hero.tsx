293ebdc3ab6f2bf0c660fa5f202be2d718cec11f
HamzaHajMtir1 <hamzahajmtir2002@gmail.com>
2024-06-25 00:07:06 +0100

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from 'react'
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const Hero = () => {
+    return (
+        <div>Hero</div>
+    );
+}
+
+export default Hero
